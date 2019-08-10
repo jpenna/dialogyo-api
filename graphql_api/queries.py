b480149d0e76e5025f63cd1a1b94376f9adc449a
Juliano Penna <julianopenna@gmail.com>
2019-08-10 12:19:55 -0700

    Separate types to their own file

--- graphql_api/queries.py
+++ graphql_api/queries.py
@@ -1,39 +1,6 @@
-from graphene import (ObjectType, String, List, Field,
-                      ID, types, NonNull)
+from graphene import (ObjectType, String, List, Field, NonNull)
 from graphql_api._data import dyo1, dyo2, reply1, reply2
-
-
-class Author(ObjectType):
-    id = ID(required=True)
-    name = String(required=True)
-    avatar = String(required=True)
-
-
-class Reply(ObjectType):
-    id = ID(required=True)
-    body = String(required=True)
-    author = Field(Author, required=True)
-
-
-privacy_description = '''
-[] - Private\n
-[*] - Public\n
-[&] - Friends\n
-['id1', 'id2'] - Selected users\n
-'''
-
-
-class Dyo(ObjectType):
-    id = ID
-    headline = String(default_value="", required=True)
-    body = String(description="The content for the post.", required=True)
-    tags = NonNull(List(NonNull(String)))
-    privacy = NonNull(List(
-                    NonNull(String)), description=privacy_description)
-    createdAt = types.datetime.DateTime(required=True)
-    author = NonNull(Author)
-    repliesList = NonNull(List(NonNull(Reply)))
-    dyosList = NonNull(List(lambda: NonNull(Dyo)))
+from graphql_api.types import Dyo, Reply
 
 
 class Query(ObjectType):
---
+++ graphql_api/types.py
@@ -0,0 +1,34 @@
+from graphene import (ObjectType, String, List, Field,
+                      ID, types, NonNull)
+
+privacy_description = '''
+[] - Private\n
+[*] - Public\n
+[&] - Friends\n
+['id1', 'id2'] - Selected users\n
+'''
+
+
+class Author(ObjectType):
+    id = ID(required=True)
+    name = String(required=True)
+    avatar = String(required=True)
+
+
+class Reply(ObjectType):
+    id = ID(required=True)
+    body = String(required=True)
+    author = Field(Author, required=True)
+
+
+class Dyo(ObjectType):
+    id = ID
+    headline = String(default_value="", required=True)
+    body = String(description="The content for the post.", required=True)
+    tags = NonNull(List(NonNull(String)))
+    privacy = NonNull(List(
+                    NonNull(String)), description=privacy_description)
+    createdAt = types.datetime.DateTime(required=True)
+    author = NonNull(Author)
+    repliesList = NonNull(List(NonNull(Reply)))
+    dyosList = NonNull(List(lambda: NonNull(Dyo)))
