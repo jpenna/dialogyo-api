3581cf70bca3b57680daf3c6200f0787a289b739
Juliano Penna <julianopenna@gmail.com>
2019-08-10 12:10:39 -0700

    Refactor: use single module for queries, mutation...

--- graphql_api/authors.py
+++
@@ -1,7 +0,0 @@
-from graphene import ObjectType, String, ID
-
-
-class Author(ObjectType):
-    id = ID(required=True)
-    name = String(required=True)
-    avatar = String(required=True)
--- graphql_api/queries.py
+++ graphql_api/queries.py
@@ -1,8 +1,19 @@
 from graphene import (ObjectType, String, List, Field,
                       ID, types, NonNull)
-from graphql_api import authors, replies
+from graphql_api._data import dyo1, dyo2, reply1, reply2
 
-from graphql_api._data import dyo1, dyo2
+
+class Author(ObjectType):
+    id = ID(required=True)
+    name = String(required=True)
+    avatar = String(required=True)
+
+
+class Reply(ObjectType):
+    id = ID(required=True)
+    body = String(required=True)
+    author = Field(Author, required=True)
+
 
 privacy_description = '''
 [] - Private\n
@@ -20,14 +31,16 @@ class Dyo(ObjectType):
     privacy = NonNull(List(
                     NonNull(String)), description=privacy_description)
     createdAt = types.datetime.DateTime(required=True)
-    author = NonNull(authors.Author)
-    repliesList = NonNull(List(NonNull(replies.Reply)))
+    author = NonNull(Author)
+    repliesList = NonNull(List(NonNull(Reply)))
     dyosList = NonNull(List(lambda: NonNull(Dyo)))
 
 
 class Query(ObjectType):
     dyo = Field(Dyo, id=String(required=True))
     dyosList = NonNull(List(NonNull(Dyo)))
+    reply = Field(Reply, id=String(required=True))
+    repliesList = List(Reply)
 
     def resolve_dyo(parent, info, id):
         return dyo1 if id == '1' else \
@@ -35,3 +48,10 @@ class Query(ObjectType):
 
     def resolve_dyosList(parent, info):
         return [dyo1, dyo2]
+
+    def resolve_reply(parent, info, id):
+        return reply1 if id == '1' else \
+                reply2 if id == '2' else None
+
+    def resolve_repliesList(parent, info, id):
+        return [reply1, reply2]
